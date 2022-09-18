400e6f3f761cfcfe359599c2f757c9bd81489849
sujithb6 <battusuji447@gmail.com>
2022-09-18 17:43:08 +0530

    lol

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { AppBar } from '@mui/material';
+
+
+export const Header = () => {
+  return (
+    <div> <AppBar position="sticky">
+<p>happy</p>
+        </AppBar>
+        </div>
+   
+  )
+}
